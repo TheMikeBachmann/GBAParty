5406d813ea4286f5cddf93d9550bb23613b76512
Mike Bachmann <bachmann.mikew@gmail.com>
2026-09-22 14:19:09 -0500

    initial SDL windows creation. painting a single color. neat.

--- GBAParty.cpp
+++ GBAParty.cpp
@@ -1,6 +1,63 @@
 #include <stdio.h>
-#include <stdbool.h> 
+#include <SDL2/SDL.h> 
 
 int main(void) {
+
+    bool quit = false;
+
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        printf("SDL_Init Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+    if (window == nullptr) {
+        printf("SDL_CreateWindow Error: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (renderer == nullptr) {
+        SDL_DestroyWindow(window);
+        printf("SDL_CreateRenderer Error: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+
+    while (!quit) {
+        SDL_Event event;
+
+        while (SDL_PollEvent(&event)) {
+
+            if (event.type == SDL_QUIT) {
+                quit = true;
+            }
+        } 
+        
+        if (SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255) != 0) {
+                printf("SDL_SetRenderDrawColor Error: %s\n", SDL_GetError());
+                SDL_DestroyRenderer(renderer);
+                SDL_DestroyWindow(window);
+                SDL_Quit();
+                return 1;
+        }
+
+            if (SDL_RenderClear(renderer) != 0) {
+                printf("SDL_RenderClear Error: %s\n", SDL_GetError());
+                SDL_DestroyRenderer(renderer);
+                SDL_DestroyWindow(window);
+                SDL_Quit();
+                return 1;
+        }
+
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0; // Notifies the OS that the program ran successfully
 }
