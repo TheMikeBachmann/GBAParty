8dec5f5a9bc1c87feac7fd6ffdb13b03304d759c
Mike Bachmann <bachmann.mikew@gmail.com>
2026-09-24 15:43:02 -0500

    SDL2 to SDL3 conversion complete. Fixed gamepad input issue

--- GBAParty.cpp
+++ GBAParty.cpp
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <SDL2/SDL.h> 
+#include <SDL3/SDL.h> 
 #include <mgba/core/core.h>
 #include <mgba/core/log.h>
 #include <mgba/gba/interface.h>
@@ -43,17 +43,17 @@ int main(void) {
     uint16_t lastControllerState = controllerState;
 
     // SDL window block
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
         printf("SDL_Init Error: %s\n", SDL_GetError());
         return 1;
     }
 
     // SDL controller subsystem initialization
-    if (SDL_InitSubSystem(SDL_INIT_GAMECONTROLLER) != 0) {
+    if (!SDL_InitSubSystem(SDL_INIT_GAMEPAD)) {
         printf("SDL_InitSubSystem Error: %s\n", SDL_GetError());
         return 1;
     }
-    std::unique_ptr<SDL_GameController, decltype(&SDL_GameControllerClose)> controller(nullptr, &SDL_GameControllerClose);
+    std::unique_ptr<SDL_Gamepad, decltype(&SDL_CloseGamepad)> controller(nullptr, &SDL_CloseGamepad);
 
     // mgba core block. Moved here so SDL_Quit isn't called before the SDL_Init
     // video buffer for GBA screen
@@ -82,23 +82,24 @@ int main(void) {
 
     // Create a window, outside main loop
     //declared this way because it is now an object that contains a pointer, rather than a pointer. cleans itself up i think because it knows how now?
-    std::unique_ptr<SDL_Window, decltype(&SDL_DestroyWindow)> window(SDL_CreateWindow("GBA Party!", 100, 100, 640, 480, SDL_WINDOW_SHOWN), &SDL_DestroyWindow);
+    std::unique_ptr<SDL_Window, decltype(&SDL_DestroyWindow)> window(SDL_CreateWindow("GBA Party!", 640, 480, 0), &SDL_DestroyWindow);
     if (window.get() == nullptr) {
         printf("SDL_CreateWindow Error: %s\n", SDL_GetError());
         return 1;
     }
     // Create a renderer. The window was just the address, this will display that address
-    std::unique_ptr<SDL_Renderer, decltype(&SDL_DestroyRenderer)> renderer(SDL_CreateRenderer(window.get(), -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC), &SDL_DestroyRenderer);
+    std::unique_ptr<SDL_Renderer, decltype(&SDL_DestroyRenderer)> renderer(SDL_CreateRenderer(window.get(), nullptr), &SDL_DestroyRenderer);
+    SDL_SetRenderVSync(renderer.get(), 1);
     if (renderer.get() == nullptr) {
         printf("SDL_CreateRenderer Error: %s\n", SDL_GetError());
         return 1;
     }
-    if (SDL_SetRenderDrawColor(renderer.get(), 255, 0, 0, 255) != 0) {
+    if (!SDL_SetRenderDrawColor(renderer.get(), 255, 0, 0, 255)) {
             printf("SDL_SetRenderDrawColor Error: %s\n", SDL_GetError());
             return 1;
         }
 
-    std::unique_ptr<SDL_Texture, decltype(&SDL_DestroyTexture)> texture(SDL_CreateTexture(renderer.get(), SDL_PIXELFORMAT_ABGR8888, SDL_TEXTUREACCESS_STREAMING, GBA_VIDEO_HORIZONTAL_PIXELS, GBA_VIDEO_VERTICAL_PIXELS), &SDL_DestroyTexture);
+    std::unique_ptr<SDL_Texture, decltype(&SDL_DestroyTexture)> texture(SDL_CreateTexture(renderer.get(), SDL_PIXELFORMAT_XBGR8888, SDL_TEXTUREACCESS_STREAMING, GBA_VIDEO_HORIZONTAL_PIXELS, GBA_VIDEO_VERTICAL_PIXELS), &SDL_DestroyTexture);
     if (texture.get() == nullptr) {
         printf("SDL_CreateTexture Error: %s\n", SDL_GetError());
         return 1;
@@ -128,40 +129,40 @@ int main(void) {
 
         while (SDL_PollEvent(&event)) {
 
-            if (event.type == SDL_CONTROLLERDEVICEADDED) {
-                controller.reset(SDL_GameControllerOpen(event.cdevice.which));
+            if (event.type == SDL_EVENT_GAMEPAD_ADDED) {
+                controller.reset(SDL_OpenGamepad(event.gdevice.which));
                 if (controller != nullptr) {
-                    printf("Controller added: %s\n", SDL_GameControllerName(controller.get()));
+                    printf("Controller added: %s\n", SDL_GetGamepadName(controller.get()));
                 }
                 else {
-                    printf("SDL_GameControllerOpen Error: %s\n", SDL_GetError());
+                    printf("SDL_OpenGamepad Error: %s\n", SDL_GetError());
                 }
             }
 
-            if (event.type == SDL_CONTROLLERDEVICEREMOVED) {
+            if (event.type == SDL_EVENT_GAMEPAD_REMOVED) {
                 controller.reset(nullptr);
-                printf("Controller removed: %s\n", SDL_GameControllerName(controller.get() ? controller.get() : nullptr));
+                printf("Controller removed: %s\n", SDL_GetGamepadName(controller.get() ? controller.get() : nullptr));
             }
 
 
-            if (event.type == SDL_QUIT) {
+            if (event.type == SDL_EVENT_QUIT) {
                 quit = true;
             }
         }
 
-        if (controller) {
+        if (controller != nullptr) {
             // Update controller state
             controllerState = 0x03FF; // Reset state
-            if (SDL_GameControllerGetButton(controller.get(), SDL_CONTROLLER_BUTTON_START)) {controllerState &= ~(1 << GBA_KEY_START);}
-            if (SDL_GameControllerGetButton(controller.get(), SDL_CONTROLLER_BUTTON_BACK)) {controllerState &= ~(1 << GBA_KEY_SELECT);}
-            if (SDL_GameControllerGetButton(controller.get(), SDL_CONTROLLER_BUTTON_DPAD_UP)) {controllerState &= ~(1 << GBA_KEY_UP);}
-            if (SDL_GameControllerGetButton(controller.get(), SDL_CONTROLLER_BUTTON_DPAD_DOWN)) {controllerState &= ~(1 << GBA_KEY_DOWN);}
-            if (SDL_GameControllerGetButton(controller.get(), SDL_CONTROLLER_BUTTON_DPAD_LEFT)) {controllerState &= ~(1 << GBA_KEY_LEFT);}
-            if (SDL_GameControllerGetButton(controller.get(), SDL_CONTROLLER_BUTTON_DPAD_RIGHT)) {controllerState &= ~(1 << GBA_KEY_RIGHT);}
-            if (SDL_GameControllerGetButton(controller.get(), SDL_CONTROLLER_BUTTON_A)) {controllerState &= ~(1 << GBA_KEY_A);}
-            if (SDL_GameControllerGetButton(controller.get(), SDL_CONTROLLER_BUTTON_B)) {controllerState &= ~(1 << GBA_KEY_B);}
-            if (SDL_GameControllerGetButton(controller.get(), SDL_CONTROLLER_BUTTON_LEFTSHOULDER)) {controllerState &= ~(1 << GBA_KEY_L);}
-            if (SDL_GameControllerGetButton(controller.get(), SDL_CONTROLLER_BUTTON_RIGHTSHOULDER)) {controllerState &= ~(1 << GBA_KEY_R);}
+            if (SDL_GetGamepadButton(controller.get(), SDL_GAMEPAD_BUTTON_START)) {controllerState &= ~(1 << GBA_KEY_START);}
+            if (SDL_GetGamepadButton(controller.get(), SDL_GAMEPAD_BUTTON_BACK)) {controllerState &= ~(1 << GBA_KEY_SELECT);}
+            if (SDL_GetGamepadButton(controller.get(), SDL_GAMEPAD_BUTTON_DPAD_UP)) {controllerState &= ~(1 << GBA_KEY_UP);}
+            if (SDL_GetGamepadButton(controller.get(), SDL_GAMEPAD_BUTTON_DPAD_DOWN)) {controllerState &= ~(1 << GBA_KEY_DOWN);}
+            if (SDL_GetGamepadButton(controller.get(), SDL_GAMEPAD_BUTTON_DPAD_LEFT)) {controllerState &= ~(1 << GBA_KEY_LEFT);}
+            if (SDL_GetGamepadButton(controller.get(), SDL_GAMEPAD_BUTTON_DPAD_RIGHT)) {controllerState &= ~(1 << GBA_KEY_RIGHT);}
+            if (SDL_GetGamepadButton(controller.get(), SDL_GAMEPAD_BUTTON_EAST)) {controllerState &= ~(1 << GBA_KEY_A);}
+            if (SDL_GetGamepadButton(controller.get(), SDL_GAMEPAD_BUTTON_SOUTH)) {controllerState &= ~(1 << GBA_KEY_B);}
+            if (SDL_GetGamepadButton(controller.get(), SDL_GAMEPAD_BUTTON_LEFT_SHOULDER)) {controllerState &= ~(1 << GBA_KEY_L);}
+            if (SDL_GetGamepadButton(controller.get(), SDL_GAMEPAD_BUTTON_RIGHT_SHOULDER)) {controllerState &= ~(1 << GBA_KEY_R);}
             if (controllerState != lastControllerState) {
                 printf("Controller state changed: 0x%04X -> 0x%04X\n", lastControllerState, controllerState);
             }
@@ -171,7 +172,7 @@ int main(void) {
 
         
 
-        if (SDL_RenderClear(renderer.get()) != 0) {
+        if (!SDL_RenderClear(renderer.get())) {
             printf("SDL_RenderClear Error: %s\n", SDL_GetError());
             return 1;
         }
@@ -180,7 +181,7 @@ int main(void) {
             std::lock_guard<std::mutex> lock(videoBufferMutex);
             SDL_UpdateTexture(texture.get(), nullptr, safeVideoBuffer.data(), GBA_VIDEO_HORIZONTAL_PIXELS * sizeof(mColor));
         }
-        SDL_RenderCopy(renderer.get(), texture.get(), nullptr, nullptr);
+        SDL_RenderTexture(renderer.get(), texture.get(), nullptr, nullptr);
         
 
         SDL_RenderPresent(renderer.get());
